0365a80c33ca8435ddf25a515f44c03620960580
pepojs <36924619+pepojs@users.noreply.github.com>
2018-03-20 19:27:47 +0100

    Zminana na szablony
    Dodanie szablonów, menu itp.

--- Drzewo_AVL/Klasa_wezel.hpp
+++ Drzewo_AVL/Klasa_wezel.hpp
@@ -3,15 +3,18 @@
 
 #include <iostream>
 #include <cstdlib>
-#define TYP_DANYCH int
+//#define TYP_DANYCH int
 
-class Wezel
+using namespace std;
+
+
+template <typename TYP_DANYCH>
+struct Wezel
 {
-    public:
 
-    Wezel* rodzic;
-    Wezel* lewy;
-    Wezel* prawy;
+    Wezel<TYP_DANYCH>* rodzic;
+    Wezel<TYP_DANYCH>* lewy;
+    Wezel<TYP_DANYCH>* prawy;
     TYP_DANYCH wartosc;
     int wr;
 
@@ -21,5 +24,54 @@ class Wezel
     void Wyswietl_Wezel();
 
 };
+
+template <typename TYP_DANYCH>
+Wezel<TYP_DANYCH>::Wezel()
+{
+    rodzic = NULL;
+    lewy = NULL;
+    prawy = NULL;
+    wartosc = 0;
+    wr = 0;
+}
+
+template <typename TYP_DANYCH>Wezel<TYP_DANYCH>::Wezel(TYP_DANYCH pocz_wartosc)
+{
+    rodzic = NULL;
+    lewy = NULL;
+    prawy = NULL;
+    wartosc = pocz_wartosc;
+    wr = 0;
+}
+
+template <typename TYP_DANYCH>void Wezel<TYP_DANYCH>::Wyswietl_Wezel()
+{
+    if(this != NULL)
+    {
+        if(this->rodzic == NULL)
+            cout<<"To jest korzen"<<endl;
+        else
+            cout<<"Wartosc wezla rodzica: "<<this->rodzic->wartosc<<endl;
+
+        cout<<"Wartosc wezla: "<<this->wartosc<<endl;
+
+        if(this->lewy == NULL)
+            cout<<"Brak lewej galezi"<<endl;
+        else
+            cout<<"Wartosc lewej galezi: "<<this->lewy->wartosc<<endl;
+
+        if(this->prawy == NULL)
+            cout<<"Brak prawej galezi"<<endl;
+        else
+            cout<<"Wartosc prawej galezi: "<<this->prawy->wartosc<<endl;
+
+        cout<<"Wspolczynnik rownowagi wezla: "<<this->wr<<endl;
+
+    }else cout<<"Drzewo jest puste"<<endl;
+}
+
+
+
+
 
 #endif
--- Drzewo_AVL/main.cpp
+++ Drzewo_AVL/main.cpp
@@ -3,7 +3,75 @@
 
 int main()
 {
-    DrzewoAVL drzewo(1);
+    DrzewoAVL<int> drzewo(1);
+
+    drzewo.Usuwanie_Wezla(drzewo.ZnajdzWezel(1));
+    int wezel;
+    char menu = 0;
+    while(menu != 'q')
+    {
+        system("cls");//Uzycie polecenia systemowego (Windows) do wyczyszczenia konsoli
+        cout<<"     MENU     "<<endl;
+        cout<<"d - dodaj nowy wezel"<<endl;
+        cout<<"u - usun wezel o danej wartosci"<<endl;
+        cout<<"p - przegladaj drzewo"<<endl;
+        cout<<"q - koniec programu"<<endl;
+        cout<<"Podaj opcje: "<<endl;
+        cin>> menu;
+        switch(menu)
+        {
+            case 'd':
+
+                cout<<"Podaj wartosc wezla do dodania: "<<endl;
+                cin>>wezel;
+                if(cin.fail()) // Zabezpieczenie przed wprowadzeniem niewlasciwych danych
+                {
+                    cin.clear();
+                    cin.ignore(1000,'\n');
+
+                    cout<<"Niewlasciwa wartosc dla wartosci wezla"<<endl;
+                    system("timeout 5");//Uzycie polecenia systemowego (Windows) do opoznienia aby mozna przeczytac komunikat
+                    continue;
+                }
+
+                drzewo.Dodawani_Wezla(wezel);
+
+            break;
+
+            case 'u':
+                cout<<"Podaj wartosc wezla do usuniecia"<<endl;
+                cin>>wezel;
+
+                if(cin.fail()) // Zabezpieczenie przed wprowadzeniem niewlasciwych danych
+                {
+                    cin.clear();
+                    cin.ignore(1000,'\n');
+
+                    cout<<"Niewlasciwa wartosc dla wartosci wezla"<<endl;
+                    system("timeout 5"); //Uzycie polecenia systemowego (Windows) do opoznienia aby mozna przeczytac komunikat
+                    continue;
+                }
+
+
+                drzewo.Usuwanie_Wezla(drzewo.ZnajdzWezel(wezel));
+
+            break;
+
+            case 'p':
+                drzewo.Przegladaj_Drzewo();
+
+            break;
+
+            case 'q':
+            break;
+
+            default:
+                cout<<"Nieznana opcja, sprobuj jeszcze raz"<<endl;
+
+            break;
+        }
+    }
+    /*DrzewoAVL<int> drzewo(1);
 
     drzewo.Dodawani_Wezla(10);
     drzewo.Dodawani_Wezla(20);
@@ -18,7 +86,7 @@ int main()
     drzewo.Przegladaj_Drzewo();
     drzewo.Usuwanie_Wezla(drzewo.ZnajdzWezel(1));
     drzewo.Przegladaj_Drzewo();
-    /*drzewo.Usuwanie_Wezla(drzewo.ZnajdzWezel(10));
+    drzewo.Usuwanie_Wezla(drzewo.ZnajdzWezel(10));
     drzewo.Przegladaj_Drzewo();
     drzewo.Usuwanie_Wezla(drzewo.ZnajdzWezel(40));
     drzewo.Przegladaj_Drzewo();
