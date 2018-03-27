a41607617ce3a4dd3987c44dd8d73a1c91744b86
pepojs <36924619+pepojs@users.noreply.github.com>
2018-03-27 23:06:11 +0200

    Usunięcie komentarzy

--- Drzewo_AVL/main.cpp
+++ Drzewo_AVL/main.cpp
@@ -84,33 +84,6 @@ int main()
             break;
         }
     }
-    /*DrzewoAVL<int> drzewo(1);
-
-    drzewo.Dodawani_Wezla(10);
-    drzewo.Dodawani_Wezla(20);
-    drzewo.Dodawani_Wezla(15);
-    drzewo.Dodawani_Wezla(30);
-    drzewo.Dodawani_Wezla(25);
-    drzewo.Dodawani_Wezla(40);
-    drzewo.Dodawani_Wezla(24);
-
-    drzewo.Przegladaj_Drzewo();
-    drzewo.Usuwanie_Wezla(drzewo.ZnajdzWezel(24));
-    drzewo.Przegladaj_Drzewo();
-    drzewo.Usuwanie_Wezla(drzewo.ZnajdzWezel(1));
-    drzewo.Przegladaj_Drzewo();
-    drzewo.Usuwanie_Wezla(drzewo.ZnajdzWezel(10));
-    drzewo.Przegladaj_Drzewo();
-    drzewo.Usuwanie_Wezla(drzewo.ZnajdzWezel(40));
-    drzewo.Przegladaj_Drzewo();
-    drzewo.Usuwanie_Wezla(drzewo.ZnajdzWezel(25));
-    drzewo.Przegladaj_Drzewo();
-    drzewo.Usuwanie_Wezla(drzewo.ZnajdzWezel(15));
-    drzewo.Przegladaj_Drzewo();
-    drzewo.Usuwanie_Wezla(drzewo.ZnajdzWezel(20));
-    drzewo.Przegladaj_Drzewo();
-    drzewo.Usuwanie_Wezla(drzewo.ZnajdzWezel(1));
-    drzewo.Przegladaj_Drzewo();*/
 
     return 0;
 }
